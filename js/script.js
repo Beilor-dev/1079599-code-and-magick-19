98096839dda0fe698eae97682ba83e2d8794b6ae
Beilor-dev <oleg.opc@gmail.com>
2020-01-27 22:00:19 +0300

    Объявляем переменные v.1.1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var fireballSize = 22;
+
+var getFireballSpeed = function(left) {
+   if (true===left) {
+    return 5;
+     } else {
+    return 2;
+  }
+}
+
+var wizardSpeed = 3;
+var wizardWidth = 70;
+
+var getWizardHeight = function() {}
