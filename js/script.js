d8a481a939149f11116343e18b370bda176012e2
Beilor-dev <oleg.opc@gmail.com>
2020-01-28 20:35:49 +0300

    cam module1-task2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var fireballSize = 22;
 
-var getFireballSpeed = function(left) {
+var getFireballSpeed = function (left) {
    if (true===left) {
     return 5;
      } else {
@@ -11,4 +11,21 @@ var getFireballSpeed = function(left) {
 var wizardSpeed = 3;
 var wizardWidth = 70;
 
-var getWizardHeight = function() {}
+var calcWizardHeight = 1.337 * wizardWidth;
+
+var getWizardHeight = function (wizardWidth) {
+    var calcWizardHeight = 1.337 * wizardWidth;
+    return calcWizardHeight;
+  },
+
+  getWizardX = function(width) {
+    return (width-wizardWidth) / 2;
+  },
+
+  getWizardY = function(height) {
+    return  height * 0.63;
+  }
+
+var WizardHeight = getWizardHeight (wizardWidth);
+
+
