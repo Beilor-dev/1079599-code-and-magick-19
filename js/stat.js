b421dce732174a4f02a68ab99e1888cced884270
Олег Горбунов <gorbunov@alvic.local>
2020-02-03 16:36:17 +0300

    cam module2-task1 v.1.2

--- js/stat.js
+++ js/stat.js
@@ -31,7 +31,7 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-var getText = function (ctx, color, font, text, x, y) {
+var textInfo = function (ctx, color, font, text, x, y) {
   ctx.fillStyle = color;
   ctx.font = font;
   ctx.fillText(text, x, y);
@@ -42,8 +42,8 @@ window.renderStatistics = function (ctx, players, times) {
   renderCLoud(ctx, BASE_X + 10, BASE_Y + 10, 'rgba(0, 0, 0, 0.7)');
   renderCLoud(ctx, BASE_X, BASE_Y, '#fff');
 
-  getText(ctx, colorBlack, fontData, textData[0], baseMargin - 20, BASE_Y * 4);
-  getText(ctx, colorBlack, fontData, textData[1], baseMargin - 20, BASE_Y * 6);
+  textInfo(ctx, colorBlack, fontData, textData[0], baseMargin - 20, BASE_Y * 4);
+  textInfo(ctx, colorBlack, fontData, textData[1], baseMargin - 20, BASE_Y * 6);
 
   var getColor = function (color) {
     for (var i = 0; i < players.length; i++) {
@@ -58,7 +58,7 @@ window.renderStatistics = function (ctx, players, times) {
     ctx.fillStyle = colorBlack;
     ctx.fillText(players[i], baseMargin + (columnMargin * i), CLOUD_HEIGHT - BASE_Y);
     ctx.fillText(Math.round(times[i]), baseMargin + (columnMargin * i), BAND_Y_POSITION + (BAND_HEIGHT * times[i] / maxTime) - BASE_Y);
-    if (players[i].length === 2) {
+    if (players[i] === 'Вы') {
       ctx.fillStyle = colorRed;
     } else {
       ctx.fillStyle = getColor(randomBlue);
@@ -66,3 +66,29 @@ window.renderStatistics = function (ctx, players, times) {
     ctx.fillRect(baseMargin + (columnMargin * i), BAND_Y_POSITION, BAND_WIDTH, (BAND_HEIGHT * times[i] / maxTime));
   }
 };
+
+
+// var maxTime = getMaxElement(times);
+
+//   for (var i = 0; i < players.length; i++) {
+//   //   ctx.fillStyle = colorBlack;
+//   //   ctx.fillText(players[i], baseMargin + (columnMargin * i), CLOUD_HEIGHT - BASE_Y);
+//   //   ctx.fillText(Math.round(times[i]), baseMargin + (columnMargin * i), BAND_Y_POSITION + (BAND_HEIGHT * times[i] / maxTime) - BASE_Y);
+//     if (players[i] === 'Вы') {
+//       ctx.fillStyle = colorRed;
+//     } else {
+//       // ctx.fillStyle = getColor(randomBlue);
+//     }
+//     ctx.fillRect(baseMargin + (columnMargin * i), BAND_Y_POSITION, BAND_WIDTH, (BAND_HEIGHT * times[i] / maxTime));
+//   }
+// };
+
+
+
+
+
+
+
+
+
+
