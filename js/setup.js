9851e9f1e98043ae0eed45a67e60327fe2556430
Beilor-dev <oleg.opc@gmail.com>
2020-02-10 21:09:58 +0300

    module3-task1 v.1.4

--- js/setup.js
+++ js/setup.js
@@ -2,16 +2,17 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COATS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var COUNT_WIZARDS = 4;
 
 var mixArray = function (array) {
-  var g;
+  var test;
   var temp;
   for (var i = array.length - 1; i > 0; i--) {
-    g = Math.floor(Math.random() * (i + 1));
-    temp = array[g];
-    array[g] = array[i];
+    test = Math.floor(Math.random() * (i + 1));
+    temp = array[test];
+    array[test] = array[i];
     array[i] = temp;
   }
   return array;
@@ -34,15 +35,16 @@ var fragment = document.createDocumentFragment();
 
 var wizards = [];
 
-var drawArrayObject = function (arrayObject, number) {
-  for (var i = 0; i < number; i++) {
-    arrayObject[i] = {
-      name: mixedNames[i] + '' + mixedSurnames[i],
+
+var createArrayObjects = function (arrayObjects) {
+  for (var i = 0; i < COUNT_WIZARDS; i++) {
+    arrayObjects[i] = {
+      name: mixedNames[i] + ' ' + mixedSurnames[i],
       coatColor: mixedCoats[i],
       eyesColor: mixedWizardEyes[i]
     };
   }
-  return arrayObject;
+  return arrayObjects;
 };
 
 var drawWizard = function (wizard) {
@@ -55,6 +57,7 @@ var drawWizard = function (wizard) {
   return wizardElement;
 };
 
+
 var buildWizardBlock = function (documentFragment, arrayObject) {
   for (var i = 0; i < arrayObject.length; i++) {
     documentFragment.appendChild(drawWizard(arrayObject[i]));
@@ -66,6 +69,6 @@ userDialog.classList.remove('hidden');
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
-drawArrayObject(wizards, 4);
+createArrayObjects(wizards);
 
 buildWizardBlock(fragment, wizards);
